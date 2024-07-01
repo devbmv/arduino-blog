dd86e1e89d23147bc97875a34b4a894c123e6e71
devbmv <maik_775@yahoo.com>
2024-07-01 18:46:16 +0100

    Add secret key

--- arduino_blog/settings.py
+++ arduino_blog/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t=juh7g$0vlys^esq2=_4lqzmu+%xwx)63b@f!e3j(!b9o=j^g"
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
